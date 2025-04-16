ebbf49a2ad99131db57468a111492b22038c69eb
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-16 19:27:05 +0700

    Create project and configure MySQL with Spring boot

---
+++ src/test/java/com/spring_boot_restful/SpringbootRestfulWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
