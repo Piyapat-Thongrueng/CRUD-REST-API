21503a67249b345d9fc2a9b979c8a1995f117b05
Piyapat <piyapat.thongrueng@gmail.com>
2025-04-20 16:19:14 +0700

    Customize Swagger API Documentation with Annotations

--- src/main/java/com/spring_boot_restful/dto/UserDto.java
+++ src/main/java/com/spring_boot_restful/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.spring_boot_restful.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "UserDto Model Information")
 @Setter
 @Getter
 @NoArgsConstructor
@@ -15,14 +17,17 @@ public class UserDto {
 
     private Long id;
 
+    @Schema(description = "User First Name")
     // User-first name should not be null or empty.
     @NotEmpty(message = "User's first name should not be null or empty.")
     private String firstName;
 
+    @Schema(description = "User Last Name")
     // User-last name should not be null or empty.
     @NotEmpty(message = "User's last name should not be null or empty.")
     private String lastName;
 
+    @Schema(description = "User Email Address")
     // User email should not be null or empty
     // Email address should be valid
     @NotEmpty(message = "User's email should not be null or empty.")
